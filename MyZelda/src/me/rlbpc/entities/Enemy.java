770558ce52e287d877e6687e7b8e058b5b593a7e
Rodolfo Lucas Bortoluzzi <qmclouca@gmail.com>
2020-10-11 13:34:43 -0300

    Criado tick do Enemy com comportamento de seguir o jogador na horizontal;

--- MyZelda/src/me/rlbpc/entities/Enemy.java
+++ MyZelda/src/me/rlbpc/entities/Enemy.java
@@ -2,12 +2,22 @@ package me.rlbpc.entities;
 
 import java.awt.image.BufferedImage;
 
+import me.rlbpc.main.Game;
+
 public class Enemy extends Entity {
 
+	private double speed = 0.6;
+	
 	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
-		// TODO Auto-generated constructor stub
 	}
 	
+	public void tick() {
+		if(x < Game.player.getX()) {
+			x+=speed;
+		} else if (x > Game.player.getX()) {
+			x-=speed;
+		}
+	}
 
 }
